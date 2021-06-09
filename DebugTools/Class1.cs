d9c4d82829baf60e18b467769f8a397fc40e18f3
Tom <toms_forever@yahoo.com>
2021-06-10 00:12:47 +0100

    Initial

---
+++ DebugTools/Class1.cs
@@ -0,0 +1,30 @@
+﻿using DebugTools;
+using MelonLoader;
+using System;
+using System.Collections;
+using UIExpansionKit.API;
+
+[assembly: MelonInfo(typeof(main), "DebugTools", "1.0.0", "Tommosaurus",
+    "https://github.com/GeekAndProud/debugtools")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+namespace DebugTools
+{
+    public class main : MelonMod
+    {
+        VRCUiManager uiman;
+        public override void OnApplicationStart()
+        {
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddSimpleButton("Reset Debug Window", () => resetDebugWindow());
+        }
+
+        private void resetDebugWindow()
+        {
+
+        }
+
+        public override void OnUpdate()
+        {
+        }
+    }
+}
